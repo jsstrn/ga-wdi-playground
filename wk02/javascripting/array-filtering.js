4626b5001b33940122b553f399e5dd8a4c8747a6
jsstrn <jessternrays@gmail.com>
2015-11-09 10:45:58 +0800

    Update files

---
+++ wk02/javascripting/array-filtering.js
@@ -0,0 +1,4 @@
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+var evenNumberFilter = number => number % 2 === 0
+var filteredEvenNubers = numbers.filter(evenNumberFilter)
+console.log(filteredEvenNubers)
---
+++ wk02/javascripting/looping-through-arrays.js
@@ -0,0 +1,3 @@
+var animals = ['cat', 'dog', 'rat']
+var pluralAnimals = animals.map(animal => animal + 's')
+console.log(pluralAnimals)
